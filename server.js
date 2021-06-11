6c12e1db13598872c204f719cbf8fc615b30305f
somaan0028 <somaanshakeel0028@gmail.com>
2021-06-11 23:13:11 +0500

    Prep for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const socket = require('socket.io');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const server = app.listen(3000, ()=>{
+const server = app.listen(process.env.PORT || 3000, ()=>{
     console.log("Listening on port 3000");
 });
 const io = socket(server);
